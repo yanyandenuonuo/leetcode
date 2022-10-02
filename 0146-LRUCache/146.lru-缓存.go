06f784cd9f6ac1cbdcf0e588f7ec35c0d48800aa
yanyandenuonuo <yanyandenuonuo@gmail.com>
2022-10-02 15:02:28 +0800

    add new solution for 0146

--- 0146-LRUCache/146.lru-缓存.go
+++ 0146-LRUCache/146.lru-缓存.go
@@ -6,24 +6,97 @@
 
 // @lc code=start
 type LRUCache struct {
-
+	size     int
+	capacity int
+	keyMap   map[int]*DLinkedNode
+	head     *DLinkedNode
+	tail     *DLinkedNode
 }
 
+type DLinkedNode struct {
+	key   int
+	value int
+	prev  *DLinkedNode
+	next  *DLinkedNode
+}
 
 func Constructor(capacity int) LRUCache {
-
+	lruCache := LRUCache{
+		size:     0,
+		capacity: capacity,
+		keyMap:   map[int]*DLinkedNode{},
+		head: &DLinkedNode{
+			key:   0,
+			value: 0,
+			prev:  nil,
+			next:  nil,
+		},
+		tail: &DLinkedNode{
+			key:   0,
+			value: 0,
+			prev:  nil,
+			next:  nil,
+		},
+	}
+	lruCache.head.next = lruCache.tail
+	lruCache.tail.prev = lruCache.head
+	return lruCache
 }
-
 
 func (this *LRUCache) Get(key int) int {
+	if keyNode, isExist := this.keyMap[key]; isExist {
+		// move to first
+		keyNode.prev.next = keyNode.next
+		keyNode.next.prev = keyNode.prev
 
+		this.head.next.prev = keyNode
+		keyNode.next = this.head.next
+		keyNode.prev = this.head
+		this.head.next = keyNode
+
+		return keyNode.value
+	}
+	return -1
 }
 
+func (this *LRUCache) Put(key int, value int) {
+	if keyNode, isExist := this.keyMap[key]; isExist {
+		// update value
+		keyNode.value = value
 
-func (this *LRUCache) Put(key int, value int)  {
+		// move to first
+		keyNode.prev.next = keyNode.next
+		keyNode.next.prev = keyNode.prev
 
+		this.head.next.prev = keyNode
+		keyNode.next = this.head.next
+		keyNode.prev = this.head
+		this.head.next = keyNode
+
+		return
+	}
+
+	if this.size == this.capacity {
+		// delete one
+		delete(this.keyMap, this.tail.prev.key)
+
+		this.tail.prev = this.tail.prev.prev
+		this.tail.prev.next = this.tail
+
+		this.size -= 1
+	}
+	keyNode := &DLinkedNode{
+		key:   key,
+		value: value,
+		prev:  this.head,
+		next:  this.head.next,
+	}
+	keyNode.next.prev = keyNode
+	this.head.next = keyNode
+
+	this.keyMap[key] = keyNode
+	this.size += 1
 }
-
 
 /**
  * Your LRUCache object will be instantiated and called as such:
