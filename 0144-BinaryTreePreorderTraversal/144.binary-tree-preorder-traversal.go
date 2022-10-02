92b85be151382758488ddc8df22342cdfb5fa028
yanyandenuonuo <yanyandenuonuo@gmail.com>
2022-10-02 20:58:34 +0800

    add new solution for 0144

--- 0144-BinaryTreePreorderTraversal/144.binary-tree-preorder-traversal.go
+++ 0144-BinaryTreePreorderTraversal/144.binary-tree-preorder-traversal.go
@@ -15,59 +15,59 @@
  *
  * Given the root of a binary tree, return the preorder traversal of its nodes'
  * values.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: root = [1,null,2,3]
  * Output: [1,2,3]
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: root = []
  * Output: []
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: root = [1]
  * Output: [1]
- * 
- * 
+ *
+ *
  * Example 4:
- * 
- * 
+ *
+ *
  * Input: root = [1,2]
  * Output: [1,2]
- * 
- * 
+ *
+ *
  * Example 5:
- * 
- * 
+ *
+ *
  * Input: root = [1,null,2]
  * Output: [1,2]
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * The number of nodes in the tree is in the range [0, 100].
  * -100 <= Node.val <= 100
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Follow up:
- * 
+ *
  * Recursive solution is trivial, could you do it iteratively?
- * 
- * 
- * 
+ *
+ *
+ *
  */
 
 // @lc code=start
@@ -80,7 +80,15 @@
  * }
  */
 func preorderTraversal(root *TreeNode) []int {
-    
+	if root == nil {
+		return []int{}
+	}
+	res := make([]int, 0)
+	res = append(res, root.Val)
+	res = append(res, preorderTraversal(root.Left)...)
+	res = append(res, preorderTraversal(root.Right)...)
+	return res
 }
+
 // @lc code=end
 
