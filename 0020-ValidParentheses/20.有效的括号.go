fee3ba988bb55a63f66bf6f42b589cc4d958cff5
yanyandenuonuo <yanyandenuonuo@gmail.com>
2022-10-03 21:18:53 +0800

    add new solution for 0020

--- 0020-ValidParentheses/20.有效的括号.go
+++ 0020-ValidParentheses/20.有效的括号.go
@@ -6,7 +6,44 @@
 
 // @lc code=start
 func isValid(s string) bool {
+	mockStack := make([]string, 0, len(s))
+	for idx := 0; idx < len(s); idx += 1 {
+		currentChar := s[idx : idx+1]
+		switch currentChar {
+		case "(":
+			fallthrough
+		case "{":
+			fallthrough
+		case "[":
+			mockStack = append(mockStack, currentChar)
+		case ")":
+			if len(mockStack) > 0 && mockStack[len(mockStack)-1] == "(" {
+				mockStack = mockStack[:len(mockStack)-1]
+			} else {
+				return false
+			}
+		case "}":
+			if len(mockStack) > 0 && mockStack[len(mockStack)-1] == "{" {
+				mockStack = mockStack[:len(mockStack)-1]
+			} else {
+				return false
+			}
+		case "]":
+			if len(mockStack) > 0 && mockStack[len(mockStack)-1] == "[" {
+				mockStack = mockStack[:len(mockStack)-1]
+			} else {
+				return false
+			}
+		default:
+			return false
+		}
+	}
+	if len(mockStack) == 0 {
+		return true
+	}
 
+	return false
 }
+
 // @lc code=end
 
