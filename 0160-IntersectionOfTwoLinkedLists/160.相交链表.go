0d62741f912a6d35276cc60591541e56f239b85d
yanyandenuonuo <yanyandenuonuo@gmail.com>
2022-10-05 19:46:24 +0800

    update solution for 0160

--- 0160-IntersectionOfTwoLinkedLists/160.相交链表.go
+++ 0160-IntersectionOfTwoLinkedLists/160.相交链表.go
@@ -13,33 +13,32 @@
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	//	solution1: hashmap
+	//  solution2: length diff: 先求2个链表长度可以得到差值diff，让较长的链表指针先走diff步逐个比较即可
+	//  solution3: 走2个指针走相同步数：走到指针末尾后指向另一个指针的头部，则A指针走了(LA-L3) + (C1-C2) + (LB1-LB2)，B指针走了(LB1-LB2) + (C1-C2) + (LA-L3)
+	//		LA1	->	LA2	->	LA3
+	//							->	C1	->	C2
+	//				LB1	->	LB2
 	if headA == nil || headB == nil {
 		return nil
 	}
 	pointA := headA
 	pointB := headB
 
-	for {
-		if pointA == pointB {
-			return pointA
-		}
-
-		if pointA.Next == nil && pointB.Next == nil {
-			return nil
-		}
-
-		if pointA.Next != nil {
+	for pointA != pointB {
+		if pointA != nil {
 			pointA = pointA.Next
-		} else if pointB.Next != nil {
+		} else {
 			pointA = headB
 		}
 
-		if pointB.Next != nil {
+		if pointB != nil {
 			pointB = pointB.Next
-		} else if pointA.Next != nil {
+		} else {
 			pointB = headA
 		}
 	}
+	return pointA
 }
 
 // @lc code=end
