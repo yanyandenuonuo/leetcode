1328e692dc7bde230fe0eec1ddaa73538f4de6ce
yanyandenuonuo <yanyandenuonuo@gmail.com>
2023-03-03 16:28:18 +0800

    add new solution for 0155

---
+++ 0155-MinStack/155.最小栈.go
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=golang
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack:    []int{},
+		minStack: []int{},
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+
+	if len(this.minStack) == 0 || val <= this.minStack[len(this.minStack)-1] {
+		this.minStack = append(this.minStack, val)
+	}
+}
+
+func (this *MinStack) Pop() {
+	val := this.stack[len(this.stack)-1]
+
+	this.stack = this.stack[:len(this.stack)-1]
+
+	if val == this.minStack[len(this.minStack)-1] {
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+
+}
+
+func (this *MinStack) GetMin() int {
+	if len(this.minStack) > 0 {
+		return this.minStack[len(this.minStack)-1]
+	}
+	return 0
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
+// @lc code=end
+
